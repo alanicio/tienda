03f2e2ccc2d423bd945b0751c348d0f8265cccd9
nonex <nonex@Nonex-DEV>
2019-07-02 10:13:09 -0500

    cargarcategorias finalizado, tambien parece actualizar categorias correctamente

--- app/Http/Controllers/Producto/CategoriaController.php
+++ app/Http/Controllers/Producto/CategoriaController.php
@@ -102,26 +102,39 @@ class CategoriaController extends Controller
                         $verificar=1;
                         $categorias=Categoria::get();
                         foreach ($categorias as $c ) {
-                            if($i>24)
-                            {
-                                if(isset($c->padre))
-                                {
-                                    if(($c->nombre==$datos[$i] && ($c->padre->nombre==$datos[$i-1] || $c->padre->nombre==$datos[$i-2])) || $datos[$i]=='---' || $datos[$i]=='Todos' || $datos[$i]=='Todas' || $datos[$i]=='Ver Todas' || $datos[$i]=='Todo')
-                                    {
-                                        $verificar=0;
-                                        break;
-                                    }
-                                }
-                                
-                            }
-                            else
-                            {
-                                if($c->nombre==$datos[$i] || $datos[$i]=='---' || $datos[$i]=='Todos' || $datos[$i]=='Todas' || $datos[$i]=='Ver Todas')
+                            if($i==26 && ($datos[25]=='---' || $datos[25]=='Todos' || $datos[25]=='Todas' || $datos[25]=='Ver Todas' || $datos[25]=='Todo')) {
+                                if(($c->nivel==2 && $c->nombre==$datos[$i] && $c->padre->nombre==$datos[$i-2]) || $datos[$i]=='---' || $datos[$i]=='Todos' || $datos[$i]=='Todas' || $datos[$i]=='Ver Todas' || $datos[$i]=='Todo')
                                 {
                                     $verificar=0;
                                     break;
                                 }
                             }
+                            elseif ($i==26) {
+                                if (($c->nivel==3 && $c->padre->nombre==$datos[$i-1] && $c->padre->padre->nombre==$datos[$i-2]) || $datos[$i]=='---' || $datos[$i]=='Todos' || $datos[$i]=='Todas' || $datos[$i]=='Ver Todas' || $datos[$i]=='Todo') {
+                                    $verificar=0;
+                                    break;
+                                }
+                            }
+                            elseif($i==25)
+                            {
+                            
+                                if(($c->nivel==2 && $c->nombre==$datos[$i] && ($c->padre->nombre==$datos[$i-1] || $c->padre->nombre==$datos[$i-2])) || $datos[$i]=='---' || $datos[$i]=='Todos' || $datos[$i]=='Todas' || $datos[$i]=='Ver Todas' || $datos[$i]=='Todo')
+                                {
+                                    $verificar=0;
+                                    break;
+                                }
+                                
+                                
+                            }
+                            else
+                            {
+                                if(($c->nombre==$datos[$i] || $datos[$i]=='---' || $datos[$i]=='Todos' || $datos[$i]=='Todas' || $datos[$i]=='Ver Todas') && $c->nivel==1)
+                                {
+                                    $verificar=0;
+                                    break;
+                                }
+                            }
+
                                 
                         }
                         if($verificar)
@@ -131,14 +144,29 @@ class CategoriaController extends Controller
                             $categoria->nivel=$i-23;
                             if($i>24)
                             {
-                                $padre=Categoria::where('nombre',$datos[$i-1])->get('id');
-                                if($padre->count())
+                                $padres=Categoria::where('nombre',$datos[$i-1])->get();
+                                if($padres->count())
                                 {
-                                    $categoria->categoria_padre=$padre->toArray()[0]['id'];
+                                    foreach ($padres as $padre) {
+                                        if($padre->nivel<$categoria->nivel)
+                                        {
+                                            $categoria->categoria_padre=$padre->id;
+                                            break;
+                                        }
+                                    }
+                                    //$categoria->categoria_padre=$padre->toArray()[0]['id'];
                                 }
                                 else
                                 {
-                                    $categoria->categoria_padre=Categoria::where('nombre',$datos[$i-2])->get('id')->toArray()[0]['id'];
+                                    $padres=Categoria::where('nombre',$datos[$i-2])->get();
+                                    $categoria->nivel=2;
+                                    foreach ($padres as $padre) {
+                                        if($padre->nivel<$categoria->nivel)
+                                        {
+                                            $categoria->categoria_padre=$padre->id;
+                                            break;
+                                        }
+                                    }
                                 }
                             }
                             $categoria->save();
@@ -146,9 +174,11 @@ class CategoriaController extends Controller
                     }                        
                 }
                 $fila++;
+                
+
                 //echo $fila.'<br>';
-                // if($fila==2000)
-                //     break;
+                if($fila==3000)
+                    break;
 
             }
             fclose($gestor);
